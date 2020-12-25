8d65cb3a52301c7425bffbd0a53e9c4cfe81f0c3
chingucoding <marcel.alex.wagner@outlook.com>
2020-12-26 00:22:30 +0100

    Fix build script

--- BuildDocs.ps1
+++ BuildDocs.ps1
@@ -1,20 +1,26 @@
 $workingDirectory = Split-Path -Path $MyInvocation.MyCommand.Path;
 
-$buildFolder = $workingDirectory + "/build";
-$docsFolder = $buildFolder + "/docs";
+$buildFolder = $workingDirectory + "\build";
+$docsFolder = $buildFolder + "\docs";
 
-if(-not (Test-Path ($buildFolder + "./Nuget.exe") -PathType Leaf))
+if(-not (Test-Path ($buildFolder + ".\Nuget.exe") -PathType Leaf))
 {
     # Download nuget.exe in case it doesn't exist
-    Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile  ($buildFolder + "/Nuget.exe")
+    Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile  ($buildFolder + "\Nuget.exe")
 }
-$NUGET_EXE = Join-Path $buildFolder "/Nuget.exe"
+$NUGET_EXE = Join-Path $buildFolder "\Nuget.exe"
 
 Invoke-Expression "$NUGET_EXE install docfx.console -Outputdirectory $docsFolder"
 
 $docfx  = (Get-ChildItem -Path $scriptFolder -Filter "docfx.exe" -Recurse).Fullname
 
-$configFile = $workingDirectory + "/docfx.json"
+if($docfx -is [array])
+{
+    # There seem to be multiple installations, lets use the first found.
+    $docfx = $docfx[0];
+}
+
+$configFile = $workingDirectory + "\docfx.json"
 
 Invoke-Expression "$docfx $configFile"
 
